ef30fd9451bddc96907f6de5b880070314cd88ec
Kevin Herrera <kevin@herrera.io>
2015-05-20 18:08:17 -0700

    Fine. We'll use a real builder.

--- Tests/Event/AbstractEventTestCase.php
+++ Tests/Event/AbstractEventTestCase.php
@@ -3,9 +3,9 @@
 namespace Box\Component\Builder\Tests\Event;
 
 use Box\Component\Builder\Builder;
+use KHerGe\File\Utility;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
 use PHPUnit_Framework_TestCase as TestCase;
-use ReflectionClass;
 
 /**
  * Manages shared characteristics of event classes.
@@ -15,18 +15,40 @@ use ReflectionClass;
 class AbstractEventTestCase extends TestCase
 {
     /**
-     * The mock builder.
+     * The builder.
      *
      * @var Builder|MockObject
      */
     protected $builder;
 
     /**
+     * The builder output file.
+     *
+     * @var string
+     */
+    private $builderFile;
+
+    /**
      * Creates a new mock builder.
      */
     protected function setUp()
     {
-        $reflection = new ReflectionClass('Box\Component\Builder\Builder');
-        $this->builder = $reflection->newInstanceWithoutConstructor();
+        $this->builderFile = tempnam(sys_get_temp_dir(), 'box-');
+
+        unlink($this->builderFile);
+
+        $this->builderFile .= '.phar';
+
+        $this->builder = new Builder($this->builderFile);
+    }
+
+    /**
+     * Destroys the builder file.
+     */
+    protected function tearDown()
+    {
+        if (file_exists($this->builderFile)) {
+            Utility::remove($this->builderFile);
+        }
     }
 }
